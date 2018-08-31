649ef79f5d6b48e405d8960e5e1d2315e5e76ca5
Behnam Hatami <behnam.hatami@gmail.com>
2018-09-01 00:04:31 +0430

    implement tsetmc crawler layer

---
+++ crawler/helper.py
@@ -0,0 +1,92 @@
+from datetime import datetime, timezone
+from io import StringIO
+import pandas as pd
+
+import requests
+import logging
+
+from crawler.models import Share, ShareHistory
+
+logging.basicConfig(level=logging.DEBUG)
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
+    'Accept': 'text/plain, */*; q=0.01',
+    'Accept-Language': 'en-GB,en;q=0.5',
+    'Origin': 'http://www.tsetmc.com',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
+
+
+def update_stock_history():
+    for share in Share.objects.all():
+        days = (datetime.now(timezone.utc) - share.last_update).days + 1 if share.last_update else 999999
+        read_stock_history(share, days=days)
+
+
+def read_stock_history(share, days):
+    print(share.__dict__, days)
+    headers = HEADERS.copy()
+    headers['Referer'] = 'http://www.tsetmc.com/Loader.aspx?ParTree=151311&i={}'.format(share.id)
+
+    params = (
+        ('i', share.id),
+        ('Top', days),
+        ('A', '0'),
+    )
+
+    share.last_update = datetime.now(timezone.utc)
+    response = requests.get('http://members.tsetmc.com/tsev2/data/InstTradeHistory.aspx', headers=headers,
+                            params=params)
+
+    if response.status_code != 200:
+        return
+
+    labels = ['date', 'high', 'low', 'close', 'last', 'first', 'yesterday', 'value', 'volume', 'count']
+    df = pd.read_csv(StringIO(response.text), sep='@', lineterminator=';', names=labels, parse_dates=['date'])
+    df = df.where((pd.notnull(df)), None)
+
+    for index, row in df.iterrows():
+        data = row.to_dict()
+        data['share'] = share
+        if ShareHistory.objects.filter(share=share, date=data['date']).exists():
+            break
+
+        ShareHistory(**data).save()
+
+    share.save()
+
+
+def read_stock_list():
+    headers = HEADERS.copy()
+    headers['Referer'] = 'http://www.tsetmc.com/Loader.aspx?ParTree=15131F'
+
+    params = (
+        ('h', '0'),
+        ('r', '0'),
+    )
+
+    response = requests.get('http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx', headers=headers, params=params)
+
+    if response.status_code != 200:
+        return
+
+    df = pd.read_csv(StringIO(response.text.split("@")[2]), sep=',', lineterminator=';', header=None)
+    df = df.where((pd.notnull(df)), None)
+
+    for index, row in df.iterrows():
+        id = row[0]
+        try:
+            share = Share.objects.get(id=id)
+        except Share.DoesNotExist:
+            share = Share()
+
+        share.eps = row[14]
+        share.symbol = row[2]
+        share.id = row[0]
+        share.description = row[3]
+
+        share.save()
--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Share(models.Model):
+    id = models.BigIntegerField(null=False, blank=False, primary_key=True)
+    symbol = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+
+    eps = models.IntegerField(null=True, blank=False)
+    last_update = models.DateTimeField(null=True)
+
+
+class ShareHistory(models.Model):
+    share = models.ForeignKey(Share, null=False, blank=False, on_delete=models.CASCADE)
+    date = models.DateField(null=False, blank=False)
+
+    first = models.IntegerField(null=False, blank=False)
+    last = models.IntegerField(null=False, blank=False)
+    close = models.IntegerField(null=False, blank=False)
+    yesterday = models.IntegerField(null=False, blank=False)
+
+    high = models.IntegerField(null=False, blank=False)
+    low = models.IntegerField(null=False, blank=False)
+
+    volume = models.BigIntegerField(null=False, blank=False)
+    count = models.BigIntegerField(null=False, blank=False)
+    value = models.BigIntegerField(null=False, blank=False)
+
