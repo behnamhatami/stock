f22b8407da68f72202f5f4fbe02596f1c8542022
Behnam Hatami <behnam@divar.ir>
2018-10-15 11:24:22 +0330

    feature: add two management command for updating list of stacks and history of them

---
+++ crawler/management/commands/update_stock_history.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+
+from django.core.management.base import BaseCommand
+
+from crawler.helper import update_stock_history
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Command(BaseCommand):
+    help = 'Update stock history'
+    requires_migrations_checks = True
+
+    def handle(self, *args, **options):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(update_stock_history())
+        loop.close()
+        self.stdout.write("Stock history updated.")
---
+++ crawler/management/commands/update_stock_list.py
@@ -0,0 +1,20 @@
+import asyncio
+import logging
+
+from django.core.management.base import BaseCommand
+
+from crawler.helper import update_stock_list
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Command(BaseCommand):
+    help = 'Update stock list'
+    requires_migrations_checks = True
+
+    def handle(self, *args, **options):
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(update_stock_list())
+            loop.close()
+            self.stdout.write("Stock list updated.")
