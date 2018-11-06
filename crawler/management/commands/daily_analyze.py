25e81ac6678555edbd9912228b2adf175bd54671
Behnam Hatami <behnam@divar.ir>
2018-11-06 12:28:43 +0330

    feature: create html report file

--- crawler/management/commands/daily_analyze.py
+++ crawler/management/commands/daily_analyze.py
@@ -11,6 +11,8 @@ from crawler.analyzers.new_comer import NewComer
 from crawler.analyzers.volume_analyzer import VolumeAnalyzer
 from crawler.models import Share
 
+import pandas as pd
+
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -27,6 +29,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         Share.DAY_OFFSET = options.get('days', 0)
+
+        row_list = []
         for share in Share.objects.all():
             if share.daily_history.shape[0] > 0 and share.daily_history.iloc[-1]['Date'] >= date.today() - timedelta(
                     days=Share.DAY_OFFSET + 1):
@@ -37,4 +41,15 @@ class Command(BaseCommand):
                         results.update(result)
 
                 if results:
+                    row_list.append({"ticker": share.ticker, **results})
                     self.stdout.write("{}".format({share.ticker: results}))
+
+
+        df = pd.DataFrame(row_list)
+
+        from django.template import loader
+        template = loader.get_template('daily_report.html')
+        html_out = template.render({'date': date.today() - timedelta(days=Share.DAY_OFFSET + 1), 'daily_report_dataframe': df.to_html()})
+
+        with open("report.html", 'w') as f:
+            f.write(html_out)
