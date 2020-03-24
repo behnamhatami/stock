fda8b59ad45e131f2fc5ac2f6f6c6e6a9c46c890
Behnam Hatami <behnam@divar.ir>
2020-03-24 16:32:42 +0430

    feature: add admin page for share and share history models

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Share, ShareDailyHistory
 
-# Register your models here.
+@admin.register(Share)
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ('ticker', 'description', 'eps', 'last_update')
+    readonly_fields = ('id', 'ticker', 'description', 'eps', 'last_update')
+    search_fields = ('ticker', 'description') 
+    
+@admin.register(ShareDailyHistory)
+class ShareDailyHistoryAdmin(admin.ModelAdmin):
+    list_display = ('share', 'date', 'tomorrow', 'volume', 'count', 'value')
+    list_filter = ('date', )
+    search_fields = ('share__ticker', 'share__description') 
