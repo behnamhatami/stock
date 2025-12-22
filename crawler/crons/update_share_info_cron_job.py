5eedc10226a249a09d7fbf56beabfce0a835ceca
Behnam Hatami <behnamhatami@gmail.com>
2025-12-22 19:39:30 +0330

    refactor: decrease running time of update share info cron job

--- crawler/crons/update_share_info_cron_job.py
+++ crawler/crons/update_share_info_cron_job.py
@@ -7,10 +7,12 @@ logger = logging.getLogger(__name__)
 
 
 class UpdateShareInfoCronJob(CronJobBase):
-    RUN_AT_TIMES = ['6:00', '18:00']
-    RETRY_AFTER_FAILURE_MINS = 30
+    RUN_ON_DAYS = [4]
+    RUN_AT_TIMES = ['06:00']
+    RETRY_AFTER_FAILURE_MINS = 60
 
-    schedule = Schedule(run_at_times=RUN_AT_TIMES, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
+    schedule = Schedule(run_on_days=RUN_ON_DAYS, run_at_times=RUN_AT_TIMES,
+                        retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
     code = 'crawler.daily_update_share_info_cron_job'  # a unique code
 
     def do(self):
